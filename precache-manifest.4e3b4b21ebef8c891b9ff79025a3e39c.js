ced79c28f6f2bda84344410a345abf75a641d40d
MaksimAgliunin <maksim-aglyunin@mail.ru>
2020-06-12 22:41:53 -0700

    Updates

---
+++ precache-manifest.4e3b4b21ebef8c891b9ff79025a3e39c.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "280988cfa2ec21a4432d3d498218590f",
+    "url": "/landing_page/index.html"
+  },
+  {
+    "revision": "f5387419c2dd9f667a4e",
+    "url": "/landing_page/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "9504fd57f05d576d9caa",
+    "url": "/landing_page/static/css/main.4d6de056.chunk.css"
+  },
+  {
+    "revision": "f5387419c2dd9f667a4e",
+    "url": "/landing_page/static/js/2.e6124851.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/landing_page/static/js/2.e6124851.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9504fd57f05d576d9caa",
+    "url": "/landing_page/static/js/main.1c6ffce5.chunk.js"
+  },
+  {
+    "revision": "5d7ae24ab899a7d9fe7a",
+    "url": "/landing_page/static/js/runtime-main.3816d1e0.js"
+  },
+  {
+    "revision": "fa8c1d9b948c9e75e005a966b5506a0c",
+    "url": "/landing_page/static/media/2.fa8c1d9b.jpg"
+  },
+  {
+    "revision": "29e7d4a607e530c88e1928dc347235da",
+    "url": "/landing_page/static/media/bandwidth-close-up-computer-connection-1148820.29e7d4a6.jpg"
+  },
+  {
+    "revision": "95611d4f19d5ae7371cb868114945ef8",
+    "url": "/landing_page/static/media/dell-motherboard-and-central-processing-unit-1432675.95611d4f.jpg"
+  },
+  {
+    "revision": "654f777effeb37d4d5c241e26202c6d3",
+    "url": "/landing_page/static/media/green-circuit-board-343457.654f777e.jpg"
+  },
+  {
+    "revision": "11d390bdf1192c5cee3f9d47771a8987",
+    "url": "/landing_page/static/media/selective-focus-photography-of-motherboard-1432673.11d390bd.jpg"
+  }
+]);
