35f75ed5affe8a377dd5fd4cafef3f852d6a4eef
MaksimAgliunin <maksim-aglyunin@mail.ru>
2020-06-23 17:58:13 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
         <div>
 
             <nav className="navbar  navbar-expand-lg navbar-dark bg-dark">
-                <a className="navbar-brand" href="#">Navbar</a>
+                <a className="navbar-brand" href="#">NAVBAR</a>
                 <button className="navbar-toggler" type="button" data-toggle="collapse"
                         data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
                         aria-expanded="false" aria-label="Toggle navigation">
